1156cc5c2eb318cb0de2153a19a21e22228d8194
kwahalf <kwanusud@gmail.com>
2026-03-20 21:21:02 +0300

    fix installation for windows

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Read the README file
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name="py-googletraffic",
